23d6911de17079e0baef6819e8632d1d138bc0b9
Sarthak Sharma <sarthak2232@gmail.com>
2024-03-06 23:02:56 +0530

    added img of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
-import { curve } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
 
 const Hero = () => {
   return (
     <Section
-      className="pt-[12rem] -mt-[-5.25]"
+      className="pt-[12rem] mt-[-5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings=""
@@ -31,6 +31,31 @@ const Hero = () => {
             with Brainwave, the open AI chat app.
           </p>
           <Button white>Get Started</Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  alt="AI"
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              width={1440}
+              height={1800}
+              alt="hero"
+              className="w-full"
+            />
+          </div>
         </div>
       </div>
     </Section>
